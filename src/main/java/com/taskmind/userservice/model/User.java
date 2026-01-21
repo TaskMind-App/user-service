056d5027dfe4e10feb18eac30aba77fa4fcc9e8c
Yasmin-Avraham <yasmin.avr96@gmail.com>
2026-01-22 00:17:52 +0200

    update pom

--- src/main/java/com/taskmind/userservice/model/User.java
+++ src/main/java/com/taskmind/userservice/model/User.java
@@ -1,10 +1,7 @@
 package com.taskmind.userservice.model;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
